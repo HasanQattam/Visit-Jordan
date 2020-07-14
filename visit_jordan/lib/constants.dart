a5ef61584da233a74e9aaa363e0b7e2ac1e967e7
Hasan Qattam <hassan.qattam@hotmail.com>
2020-07-14 23:55:15 +0300

    Added Destination Containers

--- visit_jordan/lib/constants.dart
+++ visit_jordan/lib/constants.dart
@@ -2,3 +2,9 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF27AE60);
 const kAccentColor = Color(0xFFD8ECF1);
+const kTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+  letterSpacing: 1.2,
+);
--- visit_jordan/lib/screens/homePage.dart
+++ visit_jordan/lib/screens/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:visit_jordan/constants.dart';
+import 'package:visit_jordan/models/destination_model.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,7 +14,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Color(0xFFFFFFFF),
         title: Image.asset(
-          'assets/Tourist.png',
+          'assets/images/Tourist.png',
           fit: BoxFit.cover,
         ),
         actions: <Widget>[
@@ -31,7 +32,7 @@ class _HomePageState extends State<HomePage> {
             Container(
               color: Colors.black,
               child: Image.asset(
-                'assets/home2.png',
+                'assets/images/home2.png',
                 fit: BoxFit.cover,
               ),
               height: 146.57,
@@ -39,12 +40,94 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 20,
             ),
+            Column(
+              children: <Widget>[
+                Container(
+                  color: Color(0xFFFFFFFF),
+                  height: 35.26,
+                  width: 500,
+                  padding: EdgeInsets.fromLTRB(19, 3, 3, 3),
+                  child: Text(
+                    'Suggested places',
+                    style: kTextStyle,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
             Container(
-              color: Color(0xFFFFFFFF),
-              height: 30.26,
-              width: 375,
-              child: Text(
-                'Suggested places',
+              height: 300,
+              color: Colors.blue,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: destinations.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Destination destination = destinations[index];
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    color: Colors.red,
+                    width: 210,
+                    child: Stack(
+                      alignment: Alignment.topCenter,
+                      children: <Widget>[
+                        Positioned(
+                          bottom: 15.0,
+                          child: Container(
+                            height: 120,
+                            width: 200,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    '${destination.activities.length} activities',
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    destination.description,
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                offset: Offset(0.0, 2.0),
+                                blurRadius: 6.0,
+                              ),
+                            ],
+                          ),
+                          child: Stack(
+                            children: <Widget>[
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image(
+                                  height: 180.0,
+                                  width: 180.0,
+                                  image: AssetImage(destination.imageUrl),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
           ],
