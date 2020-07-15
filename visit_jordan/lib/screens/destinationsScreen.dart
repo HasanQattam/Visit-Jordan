185693b19350d5edf720a6a32fbf4238eee2bb5e
Hasan Qattam <hassan.qattam@hotmail.com>
2020-07-15 23:06:55 +0300

    Destination Screen

---
+++ visit_jordan/lib/screens/destinationsScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:visit_jordan/models/destination_model.dart';
+
+class DestinationsScreen extends StatefulWidget {
+  final Destination destination;
+
+  DestinationsScreen({this.destination});
+
+  @override
+  _DestinationsScreenState createState() => _DestinationsScreenState();
+}
+
+class _DestinationsScreenState extends State<DestinationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
